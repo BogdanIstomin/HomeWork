365dff92baa313885e44f7eacd12feac2661e5d4
Bogdan <killah898993@gmail.com>
2023-04-12 15:19:51 +0500

    Add quad number

---
+++ Seminar1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* int num1 = 5;
+double num2 = 2.34;
+
+string word = "slovo"
+bool check = true; // или False (Неверное значение, ложь)
+
+
+Seminar 1
+Task 1: напишите программу, которая на вход принимает два числа и проверяет, является ли первое чило квадратом второго числа
+*/
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(ThreadInterruptedException a second number: );
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
