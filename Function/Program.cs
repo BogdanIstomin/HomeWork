38795a17bdd9785f1e6c3f44d32d0449c44879e5
Bogdan <killah898993@gmail.com>
2023-04-13 13:34:51 +0500

    Add func

---
+++ Function/Program.cs
@@ -0,0 +1,25 @@
+﻿int a1=34;
+int b1=13;
+int c1=37;
+
+int a2=76;
+int b2=11;
+int c2=47;
+
+int a3=32;
+int b3=2;
+int c3=6;
+
+int max = a1;
+if (b1>max) max = b1;
+if (c1>max) max = c1;
+
+if (a2>max) max = a2;
+if (b2>max) max = b2;
+if (c2>max) max = c2;
+
+if (a3>max) max = a2;
+if (b3>max) max = b2;
+if (c3>max) max = c2;
+
+Console.WriteLine (max);
