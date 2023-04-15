7bea7439e4a8441f8efe28c71e9f07ffe85ee0b9
Bogdan <killah898993@gmail.com>
2023-04-15 17:28:58 +0500

    Add HW

---
+++ Homework#1/Program.cs
@@ -0,0 +1,45 @@
+﻿/* int num1 = 5;
+double num2 = 2.34;
+
+string word = "slovo"
+bool check = true; // или False (Неверное значение, ложь)
+
+
+Seminar 1
+Task 1: напишите программу, которая на вход принимает два числа и проверяет, является ли первое чило квадратом второго числа
+*/
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+    Console.WriteLine("Yes!");
+
+else
+    Console.WriteLine("No!");
+    */
+
+    //Task 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке -N до N.
+
+/*Console.Write("Input a positive number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while (current <= number)
+{   
+    Console.Write(current + " ");
+    current++; // current = current + 1;
+}
+*/
+
+//Task 3.
+
+
+ //Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
